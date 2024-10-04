c3ece8aadd88d2789499fe3b7333028244c4b60b
R0An <roan.ho@ecpay.com.tw>
2024-10-04 16:08:37 +0800

    移除 vite.config.js 的中介軟體模式

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middlewareMode: "html", // 使用中介軟體模式
-    setupMiddlewares(middlewares) {
-      // 添加中介軟體
-      middlewares.push(
-        history({
-          disableDotRule: true,
-          htmlAcceptHeaders: ["text/html", "application/xhtml+xml"]
-        })
-      );
-      return middlewares;
-    }
-  }
+
 });
