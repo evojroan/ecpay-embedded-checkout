64f3839ac7541b9704fc4b3788a55b0ea1cd1d87
R0An <roan6903@gmail.com>
2024-10-11 17:26:19 +0800

    安裝 Redux Provider

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+//npm install @reduxjs/toolkit react-redux
+
+import { configureStore } from '@reduxjs/toolkit'
+
+export default configureStore({
+  reducer: {},
+})
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {Provider} from 'react-redux'
-//import store from './store'
+import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}>  */}
+    <Provider store={store}> 
       <App />
-      {/* </Provider> */}
+      </Provider>
    
   </React.StrictMode>,
 )
