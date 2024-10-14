2d6901de9c669985f2c250a3c51af55b31f598e1
evojroan <roan6903@gmail.com>
2024-10-14 23:06:55 +0800

    安裝 TailwindCSS

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-export default function Index(){
-    return(<>
-    <h1>首頁</h1>
-    <Link to ="/Input">開始購買</Link>
-    </>)
-
+export default function Index() {
+  return (
+    <>
+      <h1 className="font-bold">Hello</h1>
+      <Link to="/Input">開始購買</Link>
+    </>
+  );
 }
